90ed87d92d57ed156754ff57d8aa497c877760d0
buhyun <hwangbh8@naver.com>
2022-09-06 11:18:25 +0900

    mm

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,8 +18,8 @@ var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '158746', //
-  database: 'rsv715'
+  password: 'bh990731!', //
+  database: '715job'
 });
 
 // Connect
